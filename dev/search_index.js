47eb5f93d95781e66a00e8658c06df7c24891e29
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-05-20 08:35:18 +0000

    build based on 373891d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DotfileManager.jl-1","page":"Home","title":"DotfileManager.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [DotfileManager]","category":"page"}]
+}
